ef0014e0c5463b3dc7e4adf2b51cdc384fff1a6f
avicii-devops <mydadisalive@gmail.com>
2024-07-22 22:09:37 +0300

    .

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Remove old build directories
-rm -rf build dist *.egg-info
+#rm -rf build dist *.egg-info
 
 # Build the project
-python setup.py sdist bdist_wheel
+#python setup.py sdist bdist_wheel
 
 # Check the dist directory to see the created distribution files
 #ls dist
@@ -17,3 +17,10 @@ pyinstaller --onefile random_quote/cli.py
 
 # verify
 pyi-archive_viewer -l dist/cli.exe
+
+# scan
+#Use tools like pip-audit, safety, or bandit to regularly audit your dependencies for known vulnerabilities.
+#pip install pip-audit safety bandit
+pip-audit
+#safety check
+#bandit -r your_project_directory
